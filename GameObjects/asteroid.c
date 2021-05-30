8b985c2caf0f4fbdf72e8c5981570f203f77a7e2
rmit-s3718036-dylan-sbogar <dylansbogar@gmail.com>
2021-05-30 19:52:16 +1000

    Redraw asteroids
    Need to fix collision code

--- GameObjects/asteroid.c
+++ GameObjects/asteroid.c
@@ -1,28 +1,11 @@
 #include "asteroid.h"
 
 void initAsteroid(asteroid *asteroid, ship *ship) {
-    // Set the vertices of the asteroid.
-    const float r = 1.0;
-    float theta, phi;
-    vec3d vertex;
-    float step_theta = 2.0 * M_PI / ASTEROID_DIVISIONS;
-    float step_phi = M_PI / ASTEROID_DIVISIONS;
-    phi = 0;
-    for (int j = 0; j <= ASTEROID_DIVISIONS; j++, phi += step_phi) {
-            theta = 0;
-            for (int i = 0; i <= ASTEROID_DIVISIONS; i++, theta += step_theta) {
-            vertex.x = r * sinf(phi) * cosf(theta);
-            vertex.y = r * cosf(phi);
-            vertex.z = r * sinf(phi) * sinf(theta);
-            asteroid->vertices[i][j] = vertex;
-        }
-    }
-
     // Set the position of the asteroid.
     // asteroid->pos.x = 100;
     // asteroid->pos.y = 100;
     // asteroid->pos.z = 100;
-    float temp = (rand() % 360) / 360 * 2.0 * M_PI;
+    float temp = (rand() % 360) / (double)360 * 2.0 * M_PI;
     asteroid->pos.x = (ASTEROID_SPAWN_RADIUS * cosf(temp)) * ARENA_RADIUS;
     asteroid->pos.y = (ASTEROID_SPAWN_RADIUS * sinf(temp)) * ARENA_RADIUS;
     asteroid->pos.z = (ASTEROID_SPAWN_RADIUS * sinf(temp)) * ARENA_RADIUS; 
@@ -51,37 +34,13 @@ void initAsteroid(asteroid *asteroid, ship *ship) {
 }
 
 void drawAsteroid(asteroid *asteroid) {
-    // float mat_diffuse[] = { 0.0, 0.8, 0.8, 1.0 };
-    // float mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    // float mat_shininess[] = { 100.0 };
-
-    // // setup materials
-    // glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-    // glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    // glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
     glColor3f(asteroid->r, asteroid->g, asteroid->b);
-
-    // position and draw
-    // glRotatef(g_platform_rotation, 0.0, 1.0, 0.0);
-    glTranslatef(asteroid->pos.x, asteroid->pos.y, asteroid->pos.z);
+    
 
     glPushMatrix();
+    glTranslatef(asteroid->pos.x, asteroid->pos.y, asteroid->pos.z);
     glScalef(asteroid->size, asteroid->size, asteroid->size);
-    //glutSolidCube(1.0);
-    int i, j;
-    vec3d *v1, *v2;
-    for (j = 0; j < ASTEROID_DIVISIONS; j++) {
-        glBegin(GL_TRIANGLE_STRIP);
-        for (i=0; i<=ASTEROID_DIVISIONS; i++) {
-        v1 = &asteroid->vertices[i][j];
-        v2 = &asteroid->vertices[i][j+1];
-        glNormal3f(v1->x, v1->y, v1->z);
-        glVertex3f(v1->x, v1->y, v1->z);
-        glNormal3f(v2->x, v2->y, v2->z);
-        glVertex3f(v2->x, v2->y, v2->z);
-        }
-        glEnd();
-    }
+    glutSolidSphere(1.0, ASTEROID_DIVISIONS, ASTEROID_DIVISIONS);
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glPopMatrix();
 
